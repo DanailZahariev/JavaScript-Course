41a6db4626e1294fa5615d58000561613204c5e3
Danail <d.zahariev@icloud.com>
2022-04-06 22:52:22 +0300

    update

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -3,23 +3,23 @@
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 const temperatures2 = [-20, -10, -6, -1, "error", 9, 23, 17, 15, 14, 36, 5];
 
-const calcTempAmplitude = function (arr1,arr2) {
+const calcTempAmplitude = function (arr1, arr2) {
 
-  const concated = arr1.concat(arr2);
-  let min = 0;
-  let max = 0;
-  for (let i = 0; i < concated.length; i++) {
-    if (typeof concated[i] !== "number") {
-      continue;
-    }
+    const concated = arr1.concat(arr2);
+    let min = 0;
+    let max = 0;
+    for (let i = 0; i < concated.length; i++) {
+        if (typeof concated[i] !== "number") {
+            continue;
+        }
 
-    if (concated[i] < min) {
-      min = concated[i];
-    } else if (concated[i] > max) {
-      max = concated[i];
+        if (concated[i] < min) {
+            min = concated[i];
+        } else if (concated[i] > max) {
+            max = concated[i];
+        }
     }
-  }
-  return max - min;
+    return max - min;
 };
 
-console.log(calcTempAmplitude(temperatures,temperatures2));
+console.log(calcTempAmplitude(temperatures, temperatures2));

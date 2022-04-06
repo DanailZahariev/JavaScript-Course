5ef5fe2828e71340865150021e01f96be8ae9a70
Danail <d.zahariev@icloud.com>
2022-04-06 22:54:34 +0300

    update

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -5,18 +5,18 @@ const temperatures2 = [-20, -10, -6, -1, "error", 9, 23, 17, 15, 14, 36, 5];
 
 const calcTempAmplitude = function (arr1, arr2) {
 
-    const concated = arr1.concat(arr2);
+    const concat = arr1.concat(arr2);
     let min = 0;
     let max = 0;
-    for (let i = 0; i < concated.length; i++) {
-        if (typeof concated[i] !== "number") {
+    for (let i = 0; i < concat.length; i++) {
+        if (typeof concat[i] !== "number") {
             continue;
         }
 
-        if (concated[i] < min) {
-            min = concated[i];
-        } else if (concated[i] > max) {
-            max = concated[i];
+        if (concat[i] < min) {
+            min = concat[i];
+        } else if (concat[i] > max) {
+            max = concat[i];
         }
     }
     return max - min;

6c3194b798177e5ea7c1efec0e2bcda0bc20ba91
Danail <d.zahariev@icloud.com>
2022-04-06 22:41:18 +0300

    update

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,81 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+// const country = "Bulgaria";
+// const continent = "Europe";
+// let population = 6700000;
+// const isIsland = false;
+// let language;
+// // console.log(country + " " + continent + " " + population + " " + isIsland);
+// language = "Bulgarian";
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heigthJohn = 1.95;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / heigthJohn ** 2;
+// const higherBMIJohn = BMIJohn > BMIMark;
+// const higherBMIMark = BMIMark > BMIJohn;
+// console.log(BMIJohn, BMIMark)
+// console.log(higherBMIJohn, higherBMIMark)
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heigthJohn = 1.95;
+
+// const bmiMark = massMark / heightMark ** 2;
+// const bmiJohn = massJohn / heigthJohn ** 2;
+
+
+// if (bmiMark > bmiJohn) {
+//     console.log(`Mark's BMI ${bmiMark} is higher than John's ${bmiJohn}!`)
+// } else {
+//     console.log(`John's BMI ${bmiJohn} is higher than Mark's BMI${bmiMark}`)
+// }
+
+// const favourite = Number(prompt("What's you favourite number? "));
+//
+// if (favourite === 3) {
+//     console.log(favourite + " is a cool number!");
+// } else {
+//     console.log(`Your number ${favourite} is not cool`);
+// }
+
+// const dolphinsScore = (96 + 108 + 89) / 3;
+// const koalasScore = (88 + 91 + 110) / 3;
+
+// const dolphinsScore = (97 + 112 + 101) / 3;
+// const koalasScore = (109 + 95 + 123) / 3;
+
+// const dolphinsScore = (97 + 112 + 101) / 3;
+// const koalasScore = (109 + 95 + 106) / 3;
+
+// const dolphinsScore = (97 + 112 + 101) / 3;
+// const koalasScore = (10 + 10 + 10) / 3;
+
+// const dolphinsScore = (10 + 10 + 13) / 3;
+// const koalasScore = (10 + 10 + 10) / 3;
+
+// if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >= 100) {
+//     console.log(`The competition fish with a draw ${dolphinsScore} : ${koalasScore}`);
+
+// } else if (dolphinsScore > koalasScore && dolphinsScore >= 100) {
+//     console.log(`The Dolplhins wins! 🏆 With a score of ${dolphinsScore}`)
+
+// } else if (koalasScore > dolphinsScore && koalasScore >= 100) {
+//     console.log(`The Koalas wins! 🏆 With a score of ${koalasScore}`)
+
+// } else {
+//     console.log("No one wins.")
+// }   
+
+
+// const bill = 275;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+// const result = `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`;
+
+// console.log(result)

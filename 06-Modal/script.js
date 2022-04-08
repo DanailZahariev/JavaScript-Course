cd204053d4e7f2d32d1c0622b5dda5c51f1b716a
Danail <d.zahariev@icloud.com>
2022-04-08 18:27:52 +0300

    Added escape function to close window

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -16,9 +16,18 @@ const openModal = function () {
 }
 
 for (let i = 0; i < btnOpenModal.length; i++) {
+
     btnOpenModal[i].addEventListener('click', openModal);
 
     btnCloseModal.addEventListener('click', closeModal);
 
     overlay.addEventListener('click', closeModal);
 }
+
+document.addEventListener('keydown', function (event) {
+
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+
+        closeModal();
+    }
+});
